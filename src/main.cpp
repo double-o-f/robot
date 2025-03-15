56999321b52b128e5bc475fa7484197d6f50eb80
Sam <applepatato@gmail.com>
2025-03-15 18:12:10 -0500

    target uno for testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 // put function declarations here:
 int myFunction(int, int);
+uint8_t ledToggle(uint8_t);
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,13 +11,23 @@ void setup() {
   Serial.begin(9600);
   Serial.print(result);
   Serial.println(" balls");
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(ledToggle(LED_BUILTIN));
+  delay(1000);
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
+}
+
+uint8_t ledToggle(uint8_t pin) {
+  uint8_t state = !digitalRead(pin);
+  digitalWrite(pin, state);
+  return state;
 }
