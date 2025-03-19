46eadcdac9f0d8c1159647b8a3d6e3a2ccf9af26
KitchNasty02 <connorkitch10@gmail.com>
2025-03-19 00:22:14 -0500

    Implementation of ultrasonic sensor and radar set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,36 @@
 #include <Arduino.h>
-// #inlcude <Servo.h>
+#include <Servo.h>
 
 
 // --------- Function Prototypes --------- //
+void checkQuit();
 int calculateDistance(int, int);
+
+void initializeData(float[]);
+void storeRadarData(int, float);
+float getRadarData(int);
 
 
 // --------- Variables --------- //
 
+// radar servo (RS)
+Servo radar_servo;
+const int RS_PIN = 27;
+const int RS_ANGLE_MIN = 15;
+const int RS_ANGLE_MAX = 165;
+
 // ultrasonic at front of robot
 const int UF_ECHO_PIN = 23;
 const int UF_TRIG_PIN = 25;
-int UF_distance;
+float UF_distance;
+float UF_data[RS_ANGLE_MAX - RS_ANGLE_MIN + 1]; // 151 for 15 to 165 degrees
+
 
 // ultrsonic radar
 // const int UR_ECHO_PIN = ;
 // const int UR_TRIG_PIN = ;
+
+
 
 
 // --------- Setup --------- //
@@ -27,9 +42,13 @@ void setup() {
   pinMode(UF_TRIG_PIN, OUTPUT);
   pinMode(UF_ECHO_PIN, INPUT);
 
+  // initialize data arrays
+  initializeData(UF_data);
+
   Serial.begin(9600);
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  // define radar servo pin
+  radar_servo.attach(RS_PIN);
 }
 
 
@@ -38,15 +57,56 @@ void setup() {
 // runs repeatedly
 void loop() {
 
-  UF_distance = calculateDistance(UF_TRIG_PIN, UF_ECHO_PIN);
-  Serial.print("Distance: ");
-  Serial.println(UF_distance);
-  delay(10);
+  // rotate servo
+  for (int i=15; i<=165; i++) {
+    radar_servo.write(i);
+    delay(15);
+
+    // check and save distance
+    UF_distance = calculateDistance(UF_TRIG_PIN, UF_ECHO_PIN);
+    storeRadarData(i, UF_distance);
+    Serial.print(i);
+    Serial.print(" -- Distance: ");
+    Serial.println(UF_distance);
+  }
+
+  for (int i=165; i>15; i--){
+    radar_servo.write(i);
+    delay(15);
+
+    // check and save distance
+    UF_distance = calculateDistance(UF_TRIG_PIN, UF_ECHO_PIN);
+    storeRadarData(i, UF_distance);
+    Serial.print(i);
+    Serial.print(" -- Distance: ");
+    Serial.println(UF_distance);
+  }
+
+  // UF_distance = calculateDistance(UF_TRIG_PIN, UF_ECHO_PIN);
+  // Serial.print("Distance: ");
+  // Serial.println(UF_distance);
+  // delay(10);
+
+  checkQuit();
 
 }
 
 
+
+
 // --------- Functions --------- //
+
+
+// quit running if q is pressed
+void checkQuit() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == 'q') { // 'Q' stands for "quit"
+      Serial.print("Program Stopped");
+      while (true); // Infinite loop stops the program
+    }
+  }
+}
 
 // calculates distance for a given sensor
 int calculateDistance(int trig_pin, int echo_pin) {
@@ -64,6 +124,39 @@ int calculateDistance(int trig_pin, int echo_pin) {
   return distance;
 
 }
+
+
+
+// ----- Radar Data Functions ----- //
+
+
+// initialize all data to -1
+void initializeData(float array[]) {
+  for (int i = 0; i < (RS_ANGLE_MAX - RS_ANGLE_MIN + 1); i++) {
+    array[i] = -1.0; // Default value
+  }
+}
+
+// store data into array
+void storeRadarData(int angle, float distance) {
+
+  if (angle >= RS_ANGLE_MIN && angle <= RS_ANGLE_MAX) {
+    int index = angle - RS_ANGLE_MIN;
+    UF_data[index] = distance;
+  }
+
+}
+
+// will be used for visualization
+float getRadarData(int angle) {
+
+  if (angle >= RS_ANGLE_MIN && angle < RS_ANGLE_MAX) {
+    int index = angle - RS_ANGLE_MIN;
+    return UF_data[index];
+  }
+  return -1;
+}
+
 
 
 
