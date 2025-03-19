0e0b2de9ef88e7856826cd96b00b51385d7ddb1c
KitchNasty02 <connorkitch10@gmail.com>
2025-03-18 23:23:23 -0500

    Front ultrasonic implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,75 @@
 #include <Arduino.h>
+// #inlcude <Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
-uint8_t ledToggle(uint8_t);
 
+// --------- Function Prototypes --------- //
+int calculateDistance(int, int);
+
+
+// --------- Variables --------- //
+
+// ultrasonic at front of robot
+const int UF_ECHO_PIN = 23;
+const int UF_TRIG_PIN = 25;
+int UF_distance;
+
+// ultrsonic radar
+// const int UR_ECHO_PIN = ;
+// const int UR_TRIG_PIN = ;
+
+
+// --------- Setup --------- //
+
+// runs once
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  // set sensor pins
+  pinMode(UF_TRIG_PIN, OUTPUT);
+  pinMode(UF_ECHO_PIN, INPUT);
 
   Serial.begin(9600);
-  Serial.print(result);
-  Serial.println(" balls");
 
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+
+// --------- Main Loop --------- //
+
+// runs repeatedly
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(ledToggle(LED_BUILTIN));
-  delay(1000);
+
+  UF_distance = calculateDistance(UF_TRIG_PIN, UF_ECHO_PIN);
+  Serial.print("Distance: ");
+  Serial.println(UF_distance);
+  delay(10);
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+// --------- Functions --------- //
+
+// calculates distance for a given sensor
+int calculateDistance(int trig_pin, int echo_pin) {
+
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+
+  // reads echo_pin, returns sound wave travel time in ms
+  long duration = pulseIn(echo_pin, HIGH);
+  int distance = duration*0.034/2;
+  return distance;
+
 }
 
-uint8_t ledToggle(uint8_t pin) {
-  uint8_t state = !digitalRead(pin);
-  digitalWrite(pin, state);
-  return state;
-}
+
+
+
+
+
+
+
+
