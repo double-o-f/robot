b72a2a41fd9a544793bba4853610c7dfb0cbd5a6
Sam <applepatato@gmail.com>
2025-03-24 23:13:10 -0500

    just a week away

---
+++ py-stuff/slam.py
@@ -0,0 +1,10 @@
+MAP_SIZE = 8    #map size in feet
+MAP_RES = 4     #cubes per foot
+
+#7.62cm (3in (dick size))
+
+map = [None] * ((MAP_SIZE * MAP_RES) ** 2)
+print(len(map))
+
+def scan(ang, dist):
+    pass
