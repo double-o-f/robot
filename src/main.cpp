ee405b1af61f6abddc74838a96883d4bfa455575
KitchNasty02 <connorkitch10@gmail.com>
2025-03-25 22:04:09 -0500

    ultrasonic stuff

--- src/main.cpp
+++ src/main.cpp
@@ -19,14 +19,15 @@ const int RS_ANGLE_MAX = 180;
 const int RS_ANGLE_INTERVAL = 2;
 
 
-// ultrasonic radar
-const int UR_ECHO_PIN = 23;
-const int UR_TRIG_PIN = 25;
-float UR_distance;
-
 // ultrasonic at front of robot
-// const int UF_ECHO_PIN = ;
-// const int UF_TRIG_PIN = ;
+const int UF_ECHO_PIN = 29;
+const int UF_TRIG_PIN = 28;
+float UF_distance;
+
+// ultrasonic radar
+const int UR_ECHO_PIN = 30;
+const int UR_TRIG_PIN = 31;
+float UR_distance;
 
 
 
@@ -37,8 +38,8 @@ float UR_distance;
 void setup() {
   
   // set sensor pins
-  pinMode(UR_TRIG_PIN, OUTPUT);
-  pinMode(UR_ECHO_PIN, INPUT);
+  pinMode(UF_TRIG_PIN, OUTPUT);
+  pinMode(UF_ECHO_PIN, INPUT);
 
   Serial.begin(9600);
 
@@ -56,32 +57,41 @@ void loop() {
   
   // rotate servo
   for (int angle=RS_ANGLE_MIN; angle<=RS_ANGLE_MAX; angle+=RS_ANGLE_INTERVAL) {
-    radarServo.write(angle);
-    Serial.println(angle);
-    delay(20);
+    
+    // get front and radar ultrasonic
+    UF_distance = calculateDistance(UF_TRIG_PIN, UF_ECHO_PIN);
+    UR_distance = calculateDistance(UR_TRIG_PIN, UR_ECHO_PIN);
 
-    // check and save distance
-    // UR_distance = calculateDistance(UR_TRIG_PIN, UR_ECHO_PIN);
+
+    // log front ultrasonic distance
+    Serial.print("Distance: ");
+    Serial.println(UF_distance);
+
+    
+    radarServo.write(angle);
+    // Serial.println(angle);
+    delay(20);
 
     checkQuit();
   }
 
   for (int angle=RS_ANGLE_MAX; angle>RS_ANGLE_MIN; angle-=RS_ANGLE_INTERVAL){
-    radarServo.write(angle);
-    Serial.println(angle);
-    delay(20);
+    
+    // get front and radar ultrasonic
+    UF_distance = calculateDistance(UF_TRIG_PIN, UF_ECHO_PIN);
+    UR_distance = calculateDistance(UR_TRIG_PIN, UR_ECHO_PIN);
 
-    // check and save distance
-    // UR_distance = calculateDistance(UR_TRIG_PIN, UR_ECHO_PIN);
+    // log front ultrasonic distance
+    Serial.print("Distance: ");
+    Serial.println(UF_distance);
+    
+    
+    radarServo.write(angle);
+    // Serial.println(angle);
+    delay(20);
 
     checkQuit();
   }
-
-  // USE THIS FOR FRONT ONE
-  // UR_distance = calculateDistance(UR_TRIG_PIN, UR_ECHO_PIN);
-  // Serial.print("Distance: ");
-  // Serial.println(UR_distance);
-  // delay(10);
 
 }
 
