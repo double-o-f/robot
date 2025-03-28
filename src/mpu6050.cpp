0e7edf82b0f773dcd8527b5bdb242b2664609d7a
KitchNasty02 <connorkitch10@gmail.com>
2025-03-28 15:06:14 -0500

    stuff

--- src/mpu6050.cpp
+++ src/mpu6050.cpp
@@ -3,8 +3,11 @@
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 #include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
 
+
+Adafruit_MPU6050 mpu;
 
 
 
@@ -16,13 +19,14 @@ void setup() {
         Serial.println("Failed to find MPU6050 chip");
         while (1) { delay(10); }
     }
-    Serial.println("MPU6050 Found!");
+    Serial.println("MPU6050 Initialized!");
 }
 
 void loop() {
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-    Serial.print("Acceleration X: "); Serial.println(a.acceleration.x);
+    sensors_event_t accel, gyro, temp;
+    mpu.getEvent(&accel, &gyro, &temp);
+    Serial.print("Acceleration X: "); 
+    Serial.println(accel.acceleration.x);
     delay(500);
 }
 
